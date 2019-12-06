9bd73aa72cf5c8857e55afb2004b8eafbadc999e
Sakrefyohan <yohan.sakref@gmail.com>
2019-12-06 12:43:38 +0100

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        // TODO: A modifier
+        return this.apiService.getUsers();
+    }
+
+    public void generateRandomUser() {
+        // TODO: A modifier
+
+        this.apiService.generateRandomUser();
+
+        }
+
+    public void deleteUser(User user) {
+        // TODO: A modifier
+
+        this.apiService.deleteUser(user);
+
+    }
+}
